bcdf2f406c2dd2bf6c0eefc3fe18ad4151f6994c
ImranGreat1 <imrangreat1@gmail.com>
2020-01-14 13:46:49 +0100

    Added routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import store from "./redux/store";
 import { Provider } from "react-redux";
-import Container from "./components/Container";
+import Login from "./components/forms/Login";
+import UserRegister from "./components/forms/UserRegister";
 
 function App() {
   return (
     <Provider store={store}>
-      <div className="App">
-        <h1>Hello World Blog</h1>
-        <Container />
-      </div>
+      <Router>
+        <Route exact path="/">
+          <Login />
+        </Route>
+        <Route path="/">
+          <UserRegister />
+        </Route>
+      </Router>
     </Provider>
   );
 }
